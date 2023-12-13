122a53cfc1f776cc1db25eb174ec8692baababf1
abdellah585 <abdellah585@gmail.com>
2023-12-13 14:02:08 -0800

    Fix file dup

--- strfunc3.c
+++ strfunc3.c
@@ -1,88 +1,75 @@
 #include "shell.h"
 
 /**
- * _strdup - duplicates a string
- * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
+ **_strncat - concatenates two strings
+ *@str1: the first string
+ *@str2: the second string
+ *@n: the amount of bytes to be used at most
+ *Return: the concatenated string
  */
-char *_strdup(const char *str)
+char *_strncat(char *str1, char *str2, int n)
 {
-	int length = 0;
-	char *ret;
+	int i, j;
+	char *s = str1;
 
-	if (str == NULL)
-		return (NULL);
-	while (*str++)
-		length++;
-	ret = malloc(sizeof(char) * (length + 1));
-	if (!ret)
-		return (NULL);
-	for (length++; length--;)
-		ret[length] = *--str;
-	return (ret);
+	i = 0;
+	j = 0;
+	while (str1[i] != '\0')
+		i++;
+	while (str2[j] != '\0' && j < n)
+	{
+		str1[i] = str2[j];
+		i++;
+		j++;
+	}
+	if (j < n)
+		str1[i] = '\0';
+	return (s);
 }
 
 /**
- * _strcpy - copies a string
- * @str1: the string
- * @str2: the source
- *
- * Return: pointer to str1
+ **_strncpy - copies a string
+ *@str1: the string to be copied to
+ *@str2: the source string
+ *@n: the amount of chars to be copied
+ *Return: the concatenated string
  */
-char *_strcpy(char *str1, char *str2)
+char *_strncpy(char *str1, char *str2, int n)
 {
-	int i = 0;
+	int i, j;
+	char *s = str1;
 
-	if (str1 == str2 || str2 == 0)
-		return (str1);
-	while (str2[i])
+	i = 0;
+	while (str2[i] != '\0' && i < n - 1)
 	{
 		str1[i] = str2[i];
 		i++;
 	}
-	str1[i] = 0;
-	return (str1);
+	if (i < n)
+	{
+		j = i;
+		while (j < n)
+		{
+			str1[j] = '\0';
+			j++;
+		}
+	}
+	return (s);
 }
 
 /**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
+ **_strchr - locates a character in a string
+ *@str: the string to be parsed
+ *@c: the char to look for
+ *Return: (str) a pointer to the memory area str
  */
-void _puts(char *str)
+char *_strchr(char *str, char c)
 {
-	int i = 0;
+	do {
+		if (*str == c)
+			return (str);
+	} while (*str++ != '\0');
 
-	if (!str)
-		return;
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-}
-
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	static int i;
-	static char buf[WRITE_BUF_SIZE];
-
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
-	{
-		write(1, buf, i);
-		i = 0;
-	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
-	return (1);
+	return (NULL);
 }
 
